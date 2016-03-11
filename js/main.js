14c17f294b216022e403915110faad25c13cb3a6
SarahDowling <SarahandMac2@Sarahs-MacBook-Pro.local>
2016-03-11 17:01:22 -0500

    added underground veggies

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-h1
-li
-h2
+var $h1 = $('h1');
+var $ul = $('ul');
 
+$h1.html('Below ground veggies');
+$ul.addClass('undergroun-veggies');
+
+$('.more-dinos').remove();
+
+dinos.forEach(function (dino) {
+  var $li = $('<li>');
+  var $figure = $('<figure>');
+  var $img = $('<img>');
+  var $caption = $('<figcaption>');
+
+  $caption.html(dino.name);
+  $img.attr('src', 'img/' + dino.img);
+
+  $figure.append($img, $caption);
+  $li.append($figure);
+  $ul.append($li);
+});
